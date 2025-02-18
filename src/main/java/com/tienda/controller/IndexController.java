2720444c3f876712071a93fa4939bebc2130e7f8
vjulio <vjuliorc@gmail.com>
2025-02-18 12:03:31 -0600

    SEMANA 04-05
    Dos semanas en una

---
+++ src/main/java/com/tienda/controller/IndexController.java
@@ -0,0 +1,25 @@
+
+package com.tienda.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ *
+ * @author Julio
+ */
+@Controller
+public class IndexController {
+
+    @RequestMapping("/")
+    public String index(Model model) {
+        //model.addAttribute("attribute", "value");
+        return "index";
+    }
+
+    @RequestMapping("/informacion")
+    public String contacto(Model model) {
+        return "contacto";
+    }    
+}
