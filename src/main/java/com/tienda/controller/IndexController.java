03ea7c1746eea72568d94db409c84d01096d8f24
vjulio <vjuliorc@gmail.com>
2025-03-20 00:55:45 -0600

    Semana 10
    Implementación de login usando los usuarios de la base de datos

--- src/main/java/com/tienda/controller/IndexController.java
+++ src/main/java/com/tienda/controller/IndexController.java
@@ -1,6 +1,7 @@
 
 package com.tienda.controller;
 
+import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,11 +14,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class IndexController {
 
     @RequestMapping("/")
-    public String index(Model model) {
+    public String index(Model model, HttpSession session) {
         //model.addAttribute("attribute", "value");
+        Long idUsuario  = (Long)session.getAttribute("idUsuario");
         return "index";
     }
 
+    
+    
 //    @RequestMapping("/informacion")
 //    public String contacto(Model model) {
 //        return "contacto";
